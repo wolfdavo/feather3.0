5ccc710af70ce838d6d434c4656449a8ad146166
David Wolfenden <wolfdavo@gmail.com>
2019-05-20 14:48:03 -0500

    Added to navbar

--- src/components/navbar/dropdown.js
+++ src/components/navbar/dropdown.js
@@ -9,6 +9,8 @@ class Dropdown extends React.Component {
         <li><Link className="menuItem" to='/'>Home</Link></li>
         <li><Link className="menuItem" to="/classes">Classes</Link></li>
         <li><Link className="menuItem" to="/social-media-management">Social Media Management</Link></li>
+        <li><Link className="menuItem" to="/websites">Websites</Link></li>
+
         </ul>
     </nav>)
     }
