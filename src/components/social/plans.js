0fe2fb81f1a31ef3cf1049ebfeb0a390798fe9ce
David Wolfenden <wolfdavo@gmail.com>
2019-05-20 20:31:36 -0500

    smm plans page progress

---
+++ src/components/social/plans.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+export default class Plans extends React.Component {
+    render(){
+        return(
+            <div className="plans-wrapper">
+
+                <div className="plan">
+                    <h2 className="plan-name">Bare Minimum</h2>
+                    <hr/>
+                    <p className="about-plan">Keep your social media channels updated with our bare minimum plan, offering 2 posts per week for only $40/month.</p>
+                    <h3>Total posts/week: <span style={{fontWeight:'bold'}}>2</span></h3>
+                    <h3 className="plan-bonus"></h3>
+                    <h1 className="price">$39</h1>
+                </div>
+
+                <div className="plan">
+                    <h2 className="plan-name">Weekend Warrior</h2>
+                    <hr/>
+                    <p className="about-plan">Provide a little boost to the weekend rush with 4 posts week. We reccommend doing 2 on Friday and 2 on Saturday to make the most of the weekend vibes.</p>
+                    <h3>Total posts/week: <span style={{fontWeight:'bold'}}>4</span></h3>
+                    <h3 className="plan-bonus"></h3>
+                    <h1 className="price">$69</h1>
+                </div>
+
+                <div className="plan mvp">
+                    <div className="most-popular"><p>Most Popular!</p></div>
+                    <h2 className="plan-name">Steady Presence</h2>
+                    <hr/>
+                    <p className="about-plan">This is our most popular plan. It lets you focus on the weekend, but not leave everyone hanging during the work week. Fantastic option for any small business. </p>
+                    <h3>Total posts/week: <span style={{fontWeight:'bold'}}>8</span></h3>
+                    <h3 className="plan-bonus"></h3>
+                    <h1 className="price">$129</h1>
+                </div>
+
+                <div className="plan">
+                    <h2 className="plan-name">Optimal Presense</h2>
+                    <hr/>
+                    <p className="about-plan">Our best value plan! Perfect for active businesses wanting to grow an organic following and always have something new on their feed. </p>
+                    <h3>Total posts/week: <span style={{fontWeight:'bold'}}>14</span></h3>
+                    <h3 className="plan-bonus"></h3>
+                    <h1 className="price">$199</h1>
+                </div>
+
+            </div>
+        )
+    }
+}
--- src/pages/social-media-management.js
+++ src/pages/social-media-management.js
@@ -1,6 +1,10 @@
 import React from "react"
 import { Helmet } from "react-helmet" 
 import Layout from "../components/layout"
+import Img from 'gatsby-image'
+import { graphql } from 'gatsby'
+import Plans from '../components/social/plans'
+
 
 export default class Homepage extends React.Component{
 
@@ -11,8 +15,36 @@ export default class Homepage extends React.Component{
             <Helmet>
                 <title>Feather Consulting - Social Media Management</title>
             </Helmet>
-            
+            <div className="classes-body">
+            <h1 className="classes-header">Social Media Management Plans</h1>
+            <div className="smm-banner">
+                <div className="banner-content">
+                <div className="img-wrapper"><Img fluid={this.props.data.socialMediaImage.childImageSharp.fluid}/>
+                </div>
+                
+                <h3 className="smm-banner-text">Our aim at Feather is to take weight off your shoulders. One of the ways we do that is helping you manage your social media. <br/> Once a month, one of our team members will come in to take photos of your business/products, write captions, schedule posts, and find ways to increase organic growth. Take a look below at the four different plans we have available, each one varying in post frequency.</h3>
+                </div>
+            </div>
+
+            <Plans/>
+            </div>
             </Layout>
         )
     }
 }
+
+
+
+export const query = graphql`
+  query {
+
+    socialMediaImage: file(relativePath: { eq: "social.png" }) {
+        childImageSharp {
+        # Specify the image processing specifications right in the query.
+        # Makes it trivial to update as your page's design changes.
+        fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid_tracedSVG
+        }
+        }
+    }
+}`
